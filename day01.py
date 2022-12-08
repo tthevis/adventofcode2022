83e544188f0b3ce355d6409d827805d478b9bf0f
Thomas Thevis <thomas@no_email.com>
2022-12-08 22:21:04 +0100

    initial commit cntaining the execution framework, blackbox test and algorithm and data for day 3 part 1

--- day01.py
+++
@@ -1,8 +0,0 @@
-file = open("input", "r")
-data = file.read()
-file.close()
-
-result = []
-for block in data.split("\n\n"):
-    result.append(sum(list(map(lambda x: int(x.strip()), block.split("\n")))))
-print(sum(sorted(result, reverse=True)[0:3]))
