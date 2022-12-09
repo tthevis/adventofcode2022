2dbbfe7a0677caa00f91cc458f9a622e64659c2c
Thomas Thevis <thomas@no_email.com>
2022-12-09 18:46:03 +0100

    added implementation + data for day3 part 2 assignment

--- day3.py
+++ day3.py
@@ -9,6 +9,10 @@ class Day3(Day):
         if 'A' <= x <= 'Z':
             return ord(x) - 38
 
+    @staticmethod
+    def intersection(l1: list, l2: list, l3: list):
+        return list(set([x for x in l1 if x in l2 and x in l3]))
+
     def part1(self):
         result = []
         for line in self.input:
@@ -17,3 +21,9 @@ class Day3(Day):
             result += list(set([self.priority(c) for c in line[:split_pos] if c in line[split_pos:]]))
 
         return sum(result)
+
+    def part2(self):
+        res = 0
+        for i in range(0, len(self.input), 3):
+            res += sum(list(map(lambda x: Day3.priority(x), Day3.intersection(self.input[i].strip(), self.input[i+1].strip(), self.input[i+2].strip()))))
+        return res
