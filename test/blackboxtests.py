78c8d3c715d90ccb3b89ddd5423286bb5b0f341b
Thomas Thevis <thomas@no_email.com>
2022-12-11 20:04:58 +0100

    make use of SubTest functionality for individual results

--- test/blackboxtests.py
+++ test/blackboxtests.py
@@ -4,37 +4,38 @@
 #
 
 import unittest
-from pathlib import Path
-import os
 import re
+import os
+from pathlib import Path
 
 import adventofcode2022
 
 
-class MyTestCase(unittest.TestCase):
+class BlackboxTest(unittest.TestCase):
+    longMessage = True
 
-    def parse_day_part(self, path_name):
-        pattern = re.compile('.*_(\d+)_(\d+)')
-        match = pattern.search(path_name)
+    @staticmethod
+    def parse_day_part(path_name):
+        filename_pattern = re.compile(".*_(\\d+)_(\\d+)")
+        match = filename_pattern.search(path_name)
         day, part = match.groups()
         return day, part
 
     def test_expected_results(self):
         # need to change working directory in order to resolve paths from the main framework
         os.chdir("../")
-        pathlist = Path("test/expected_results").rglob('result_*')
-        for path in pathlist:
-            # because path is object not string
-            path_in_str = str(path)
-            day, part = self.parse_day_part(path_in_str)
-            result = adventofcode2022.main(int(day), int(part))
 
-            f = open(path_in_str, "r")
-            expected_result = f.read()
-            f.close()
-            print("run black box test for day {} part {}".format(day, part))
-            assert str(result) == str(expected_result), "result '{}' does not match expected result '{}'".format(result, expected_result)
-            print("OK")
+        path_list = Path("test/expected_results").rglob('result_*')
+        for path in path_list:
+            path_to_expected_result = str(path)
+            day, part = BlackboxTest.parse_day_part(path_to_expected_result)
+
+            with self.subTest(expected_result=path_to_expected_result):
+                result = adventofcode2022.main(int(day), int(part))
+                f = open(path_to_expected_result, "r")
+                expected_result = f.read()
+                f.close()
+                self.assertEqual(str(result), expected_result)
 
 
 if __name__ == '__main__':
